dbda136f661ef5b85a9591e6803ab7f9fdcc907f
yaramiu <yaramiu@gmail.com>
2024-07-15 01:48:23 -0400

    Fix vscode eslint extension error

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.jest } } },
   pluginJs.configs.recommended,
-  { plugins: ["jest"] },
 ];
